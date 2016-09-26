04a15fac69b85a61d25e4e35f24fd55fecbb7279
acp1997 <pradhumn1997goyal@gmail.com>
2016-09-26 18:57:39 +0530

    added sources and basic folders
    1. sources for materialize and jquery.
    2. separate folders for css, jquery files and images.

---
+++ home_subhead.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="sources/materialize/css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        
+        <title></title>
+        <link rel="shortcut icon" href="images/">
+        
+        <!--CSS for this file-->
+        <link rel="stylesheet" href="css/">
+    
+    </head>
+    <body>
+
+    
+
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="sources/jquery-3.0.0.min.js"></script>
+        <script type="text/javascript" src="sources/materialize/js/materialize.min.js"></script>
+        <!--Jquery for this file-->
+        <script type="text/javascript" src="jquery/"></script>
+    </body>
+</html>
