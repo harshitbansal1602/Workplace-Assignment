b57b983d0ffc173d814c851c22b81aa04f389642
acp1997 <pradhumn1997goyal@gmail.com>
2016-09-28 14:31:00 +0530

    added home file for head
    1. basic template of home for head.

--- home_head.php
+++ home_head.php
@@ -12,19 +12,22 @@
 
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
+	
 	<title></title>
 	<link rel="shortcut icon" href="images/">
-
+	
 	<!--CSS for this file-->
 	<link rel="stylesheet" href="css/">
-
+	
 </head>
 <body>
 	<header>
 		<div class="row">
-			<div class="col s8">Welcome</div>
+			<div class="col s6">Welcome</div>
 			<div class="col s2">All Tasks</div>
+			<div class="col s2">
+				<a class="modal-trigger" href="#modal1">Create Task</a>
+			</div>
 			<div class="col s2">Logout</div>
 		</div>
 	</header>
@@ -54,11 +57,27 @@
 	<footer style="text-align: center;">
 		Harshit Bansal &bull; Pradhumn Goyal
 	</footer>
+	
+	
+	<!-- Modal1 Structure -->
+	<div id="modal1" class="modal modal-fixed-footer">
+		<div class="modal-content">
+			<h4>Modal Header</h4>
+			<p>A bunch of text</p>
+		</div>
+		<div class="modal-footer">
+			<a href="#!" class="modal-action modal-close btn-flat ">Create</a>
+			<a href="#!" class="modal-action modal-close btn-flat ">Cancel</a>
+		</div>
+	</div>
 
 	<!--Import jQuery before materialize.js-->
 	<script type="text/javascript" src="sources/jquery-3.0.0.min.js"></script>
 	<script type="text/javascript" src="sources/materialize/js/materialize.min.js"></script>
 	<!--Jquery for this file-->
 	<script type="text/javascript" src="jquery/"></script>
+	<script>
+		$('.modal-trigger').leanModal();
+	</script>
 </body>
 </html>
